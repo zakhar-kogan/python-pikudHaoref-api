807cf36726c065149ab76fff3ce9afb321bae85d
Zakhar Kogan <36503576+zaharkogan@users.noreply.github.com>
2024-10-15 12:13:20 +0300

    1. Updated up to Python 3.13 2. Small URL/request fixes 3. Error handling for history requests 4. .yml + requirements.txt + .gitignore (are they even needed?)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
   packages = ['tzevaadom'],
   version = '1.0',
   license='MIT',
-  description = 'A Python library for Pikud Haoref's unofficial rocket alert API.',
+  description = 'A Python library for Pikud Haoref\'s unofficial rocket alert API.',
   long_description=long_description,
   long_description_content_type='text/markdown',
   author = 'Itai Guli',
@@ -28,6 +28,11 @@ setup(
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8'
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
+    'Programming Language :: Python :: 3.13',
   ],
 )
